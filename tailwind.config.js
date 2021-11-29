a64b6f1062424f7ed0be1b9ec7e7d769729fe427
Phillip Cunningham <phillipcunningham@Phillips-MBP.home>
2021-11-28 20:28:58 -0600

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  corePlugins: {
+    dropShadow: true
+  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    minWidth: {
+      'button': '175'
+    },
+    fontFamily: {
+      'heading': ['montserrat', 'sans-serif'],
+      'body': ['Fira Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
