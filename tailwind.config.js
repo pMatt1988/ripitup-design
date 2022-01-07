c1dc3eb5925677784b7377f139cc3d3b498a565e
mc <mc@dialogs.com>
2022-01-07 12:40:51 -0600

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    minWidth: {
-      'button': '175'
-    },
     fontFamily: {
       'heading': ['montserrat', 'sans-serif'],
       'body': ['Fira Sans', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      screens: {
+        'msm': {'max': '639px' }
+      },
+      minWidth: {
+        'button': '175'
+      }
+    },
   },
   variants: {
     extend: {},
